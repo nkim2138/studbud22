5117efa042f1ae8b9c5e4fee0175a147f65eecfc
nkim2138 <100391122+nkim2138@users.noreply.github.com>
2022-05-12 01:15:33 +1000

    commit changes

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const express = require('express');
 //call express function
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static('dist'));
 
 //when user lands on root URL then we will run a function which gets the request that user made
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/public/index.html')
+    res.sendFile(__dirname + '/dist/index.html')
 })
 
 let server = app.listen(8888, function(){
