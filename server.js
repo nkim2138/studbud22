283711dba58445e8717c38a282d567fb2176a2d7
nkim2138 <100391122+nkim2138@users.noreply.github.com>
2022-05-11 23:09:31 +1000

    setup server using express.js

---
+++ server.js
@@ -0,0 +1,17 @@
+//import package using require syntax
+const express = require('express');
+
+//call express function
+const app = express();
+
+app.use(express.static('public'));
+
+//when user lands on root URL then we will run a function which gets the request that user made
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/public/index.html')
+})
+
+let server = app.listen(8888, function(){
+    console.log("App server is running on port 8888");
+});
+
