977826ad1ac725219a35b231106efd76b400eaca
nkim2138 <100391122+nkim2138@users.noreply.github.com>
2022-05-14 03:37:11 +1000

    top nav

--- public/script.js
+++ public/script.js
@@ -1,1 +1,21 @@
 //client side code which links back to HTML
+
+// -----------------------------------KANBAN BOARD------------------------------------
+// ------CREATE/DELETE A TASK FORM-------
+
+// ------CREATE/DELETE A COLUMN FORM-------
+
+
+// --------------------------------RESOURCES-----------------------------------
+// ------ADD A NEW RESOURCE FORM-------
+
+// ------ADD A NEW SUB-GROUP FORM-------
+
+
+// ----------------------------POMODORO + STOPWATCH------------------------------
+// ------POMODORO-------
+
+// ------STOPWATCH-------
+
+
+// ----------------------------MUSIC PLAYER------------------------------
